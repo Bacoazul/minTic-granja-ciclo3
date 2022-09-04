d8b6c6bc9c0d6e7e7b8c3bad2a2352e3d98a67f8
Pedro Pablo Gallego Pinzon <pedropablo1996@gmail.com>
2022-09-04 11:24:41 -0500

    new empty project with postgress conection

---
+++ src/main/java/com/co/edu/udea/granjamintic/GranjaMinticApplication.java
@@ -0,0 +1,13 @@
+package com.co.edu.udea.granjamintic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GranjaMinticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GranjaMinticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/edu/udea/granjamintic/GranjaMinticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.edu.udea.granjamintic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GranjaMinticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
