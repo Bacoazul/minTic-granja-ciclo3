46565475bb9d5528acf53aaa3332468fbf79bbd3
Pedro Pablo Gallego Pinzon <pedropablo1996@gmail.com>
2022-09-23 13:29:10 -0500

    no message

--- src/main/java/com/co/edu/udea/granjamintic/servicio/PersonaServicio.java
+++ src/main/java/com/co/edu/udea/granjamintic/servicio/PersonaServicio.java
@@ -3,7 +3,6 @@ package com.co.edu.udea.granjamintic.servicio;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.co.edu.udea.granjamintic.modelo.Persona;
